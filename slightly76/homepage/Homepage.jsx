218efaa2f5d6e7d943c1f52ac009b59f02649337
Chris Askew <caskew@hotmail.com>
2025-05-12 17:42:30 +0100

    making mobile look better and breaking things

--- slightly76/homepage/Homepage.jsx
+++ slightly76/homepage/Homepage.jsx
@@ -1,108 +1,113 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './homepage.css';
 
 function Homepage() {
 	const panelsRef = useRef([]);
+	const [activeIndex, setActiveIndex] = useState(null);
 
 	useEffect(() => {
-		const observer = new IntersectionObserver(
-			(entries) => {
-				entries.forEach((entry) => {
-					entry.target.classList.toggle('in-view', entry.isIntersecting);
-				});
-			},
-			{ threshold: 0.6 }
-		);
+		const handleScroll = () => {
+			const viewportCenter = window.innerHeight / 2;
 
-		panelsRef.current.forEach((panel) => {
-			if (panel) observer.observe(panel);
-		});
-
-		return () => {
-			panelsRef.current.forEach((panel) => {
-				if (panel) observer.unobserve(panel);
+			const distances = panelsRef.current.map((panel) => {
+				if (!panel) return Infinity;
+				const rect = panel.getBoundingClientRect();
+				const panelCenter = rect.top + rect.height / 2;
+				return Math.abs(viewportCenter - panelCenter);
 			});
+
+			const closestIndex = distances.indexOf(Math.min(...distances));
+			setActiveIndex(closestIndex);
 		};
+
+		window.addEventListener('scroll', handleScroll, { passive: true });
+		handleScroll();
+
+		return () => window.removeEventListener('scroll', handleScroll);
 	}, []);
 
 	return (
 		<div className='homepage'>
-			<div className='panel' ref={(el) => (panelsRef.current[0] = el)}>
+			<div
+				ref={(el) => (panelsRef.current[0] = el)}
+				className={`panel ${activeIndex === 0 ? 'in-view' : ''}`}
+			>
 				<div className='header-panel'>
-					<div className='crt-image-wrapper'>
-						<img className='crt-image base' src='./assets/me.jpg' alt='Me' />
-						<img
-							className='crt-image hover'
-							src='./assets/mehover.jpg'
-							alt='Me Hover'
-						/>
+					<div className='header-top'>
+						<div className='image-wrapper'>
+							<img
+								src='./assets/me.jpg'
+								className='hover-image base'
+								alt='Me'
+							/>
+							<img
+								src='./assets/mehover.jpg'
+								className='hover-image hover'
+								alt='Me Hovering'
+							/>
+						</div>
+
+						<div className='header-text'>
+							<h1 className='name-heading'>Chris Askew</h1>
+							<h2 className='accent'>Junior Software</h2>
+							<h2 className='accent'>& Game Dev</h2>
+						</div>
 					</div>
-					<div className='header-text'>
-						<h1 className='name-heading'>Chris Askew</h1>
-						<h2 className='accent'>Junior Software</h2>
-						<h2 className='accent'>& Game Dev</h2>
+					<div className='header-slug'>
+						<p>
+							At 49 it feels weird calling myself a Junior <i>anything</i>, but
+							here we are.
+						</p>
 					</div>
 				</div>
-				<p>
-					At 49 it feels weird calling myself a Junior <i>anything</i>, but here
-					we are.
-				</p>
 			</div>
 
-			<div className='panel' ref={(el) => (panelsRef.current[1] = el)}>
-				<h3>/coding_skillset.</h3>
-				<h3>JavaScript | Full Stack | Phaser</h3>
+			<div
+				ref={(el) => (panelsRef.current[1] = el)}
+				className={`panel ${activeIndex === 1 ? 'in-view' : ''}`}
+			>
+				<h3>let codingSkillset =</h3>
+				<h3>{'{JavaScript, Full Stack, Phaser}'}, </h3>
 				<h3>
-					Test Driven Development | Pair Programming | SCRUM Methodologies |
-					Vibe Coding <i>(whisper it)</i>
+					{'{Test Driven Development, Pair Programming, SCRUM Methodologies & '}{' '}
+					<i>(whisper it)</i> Vibe Coding{'}'}
 				</h3>
 			</div>
 
 			<div
-				className='panel text-content'
 				ref={(el) => (panelsRef.current[2] = el)}
+				className={`panel ${activeIndex === 2 ? 'in-view' : ''}`}
 			>
 				<p className='muted'>
-					/about_me.
+					//about_me
 					<br />
 					<br />
-					Tackling the spectre of redundancy head-on by pivoting towards
-					Software Dev, after over 20 years experience providing tech support to
-					an Animation & Design BA(Hons) Degree here in the UK. As a result of
-					my work in Education I'm comfortable imparting Hardware & Software
-					Support for PC & Mac, performing Technical Demonstration, undertaking
-					Video & Audio Editing, assisting Lighting & Camera Operation etc.
-					<br />
-					<br />I had a great rapport with the students and offered pastoral
-					support too, revelling in voice acting & narration roles for student
-					film & radio projects when the opportunity arose. I'm also comfortable
-					working with creative disciplines such as Illustration, Graphic
-					Design, Film/TV/Radio. I guess I enjoyed the creative side of things
-					more than the generic IT support. As you can probably tell, I tend to
-					roll up my sleeves and have a go <i>regardless</i> of the challenge.
-					The classic 'Jack of all'.
-					<br />
-					<br />
-					So, I reckon I'm probably half way through my career journey (fingers
-					crossed!) and I'm ready for a fresh challenge in coding. It's
-					something I loved as a kid and would have remained on that career path
-					had I not been directed otherwise by parents & teachers who didn't
-					really <i>get it</i> in the late 80's. Now, thanks to an intense 13
-					week bootcamp in JavaScript at{' '}
+					Faced with redundancy, I've grasped the opportunity to pivot towards
+					Software Dev after 20 years of experience providing tech support to an
+					Animation & Design BA(Hons) Degree programme in the UK. I'm
+					comfortable with PC & Mac support, technical demonstrations,
+					video/audio editing, and more.
+				</p>
+				<br />
+				<br />
+				<p>
+					I loved providing pastoral care to the students, doing voice acting
+					for student films, and supporting creative disciplines like Film/TV
+					and Graphic Design.
+				</p>
+				<p>
+					Now I'm returning to code thanks to a 13-week bootcamp at{' '}
 					<a
 						href='https://northcoders.com/'
 						target='_blank'
 						rel='noopener noreferrer'
 					>
 						Northcoders
-					</a>{' '}
-					I'm stood on the precipice of what feels like an exciting career
-					redemption.
-				</p>
-				<p>
-					I'm particularly passionate about games <i>whilst</i> being able to
-					pay the bills, so please click <a href='#'>here</a> to stop me from
-					playing games so much and maybe employ me.
+					</a>
+					. I find myself particularly passionate about games <i>and</i> being
+					able to pay the bills, so please click <a href='#'>here</a> to stop me
+					from playing games so much and discuss employing me.
 				</p>
 				<button>This is a button</button>
 			</div>
