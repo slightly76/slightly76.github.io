d25dc2f6d028adaa6413fc07f07ebdb090c6bd6d
Chris Askew <caskew@hotmail.com>
2025-06-03 16:13:58 +0100

    attempt to fix paths again2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-	base: '/slightly76.github.io/',
+	base: '/',
 	plugins: [react(), svgr()],
 });
