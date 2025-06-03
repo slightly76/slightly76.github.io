e87434c3d33850debae35371fcfc99796e19b409
Chris Askew <caskew@hotmail.com>
2025-06-03 16:30:58 +0100

    changed base again in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-	base: '/',
+	base: '/slightly76.github.io/',
 	plugins: [react(), svgr()],
 });
